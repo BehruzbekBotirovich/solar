ff6b15794cb1ccebcf23f323333854702c462c8c
BehruzbekBotirovich <behruzbekbotirovich@gmail.com>
2024-08-06 17:01:36 +0500

    Updated specific file

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main-container">
-
+        <h1>bu karta bizni filiallar</h1>
         <h1>bu yerda map </h1>
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2994.959473064018!2d69.34871427360693!3d41.35323459811674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef47140cbcd4f%3A0x69c9e6e30a920e30!2sEcobozor!5e0!3m2!1sru!2s!4v1722502828928!5m2!1sru!2s"
